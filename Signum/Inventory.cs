da63f8535eff1fc43eb0179a7ac2727fb7ba80d6
leefine02 <kfadmin@LFINE-KF10-ORCH>
2023-08-17 19:09:52 +0000

    Signum

---
+++ Signum/Inventory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Keyfactor.Logging;
+using Keyfactor.Orchestrators.Extensions;
+using Keyfactor.Orchestrators.Extensions.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Keyfactor.Extensions.Orchestrator.Signum
+{
+    public class Inventory : IInventoryJobExtension
+    {
+        public string ExtensionName => "";
+
+        public IPAMSecretResolver _resolver;
+
+        public JobResult ProcessJob(InventoryJobConfiguration config, SubmitInventoryUpdate submitInventory)
+        {
+            ILogger logger = LogHandler.GetClassLogger(this.GetType());
+            logger.LogDebug($"Begin {config.Capability} for job id {config.JobId}...");
+            logger.LogDebug($"Server: {config.CertificateStoreDetails.ClientMachine}");
+            logger.LogDebug($"Store Path: {config.CertificateStoreDetails.StorePath}");
+            logger.LogDebug($"Job Properties:");
+            foreach (KeyValuePair<string, object> keyValue in config.JobProperties ?? new Dictionary<string, object>())
+            {
+                logger.LogDebug($"    {keyValue.Key}: {keyValue.Value}");
+            }
+
+            List<CurrentInventoryItem> inventoryItems = new List<CurrentInventoryItem>();
+
+            try
+            {
+                string userName = PAMUtilities.ResolvePAMField(_resolver, logger, "Server User Name", config.ServerUsername);
+                string userPassword = PAMUtilities.ResolvePAMField(_resolver, logger, "Server Password", config.ServerPassword);
+            }
+            catch (Exception ex)
+            {
+                return new JobResult() { Result = Keyfactor.Orchestrators.Common.Enums.OrchestratorJobStatusJobResult.Failure, JobHistoryId = config.JobHistoryId, FailureMessage = "Custom message you want to show to show up as the error message in Job History in KF Command" };
+            }
+
+            try
+            {
+                submitInventory.Invoke(inventoryItems);
+                return new JobResult() { Result = Keyfactor.Orchestrators.Common.Enums.OrchestratorJobStatusJobResult.Success, JobHistoryId = config.JobHistoryId };
+            }
+            catch (Exception ex)
+            {
+                return new JobResult() { Result = Keyfactor.Orchestrators.Common.Enums.OrchestratorJobStatusJobResult.Failure, JobHistoryId = config.JobHistoryId, FailureMessage = "Custom message you want to show to show up as the error message in Job History in KF Command" };
+            }
+        }
+    }
+}
---
+++ Signum/SignumClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+using SignumReference;
+
+namespace Keyfactor.Extensions.Orchestrator.Signum
+{
+    internal class SignumClient
+    {
+        private string EndpointURL { get; set; }
+        private string UserId { get; set; }
+        private string Password { get; set; }
+
+        internal SignumClient(string endpointURL, string userId, string password)
+        {
+
+        }
+    }
+}
